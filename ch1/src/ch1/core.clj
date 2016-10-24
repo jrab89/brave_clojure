0dfd54728b0d4693a8638019de5be7934a93504f
Jeff Rabovsky <jrab89@gmail.com>
2016-10-23 23:10:22 -0500

    Hello Clojure

---
+++ ch1/src/ch1/core.clj
@@ -0,0 +1,19 @@
+(ns ch1.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+(def default-things ["How are you?"
+                     "asdf"
+                     "xyz"])
+
+(defn say-a-thing [to-who
+                   & {:keys [index things]
+                      :or {index 0 things default-things}}]
+  (str "Hello, "
+       to-who
+       "! "
+       (get things index)))
