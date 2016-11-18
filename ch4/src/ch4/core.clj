b77ca7b08e23ac6613c8a78451ec19247337c6e3
Jeff Rabovsky <jrab89@gmail.com>
2016-11-18 17:38:56 -0600

    finished ch4 exercises

---
+++ ch4/src/ch4/core.clj
@@ -0,0 +1,60 @@
+(ns ch4.core
+  (:gen-class))
+
+(def filename "suspects.csv")
+(def vamp-keys [:name :glitter-index])
+
+(defn str->int
+  [str]
+  (Integer. str))
+
+(def conversions {:name identity
+                  :glitter-index str->int})
+
+(defn convert
+  [vamp-key value]
+  ((get conversions vamp-key) value))
+
+(defn parse
+  "Convert a CSV into rows of columns"
+  [string]
+  (map #(clojure.string/split % #",")
+       (clojure.string/split string #"\n")))
+
+(defn mapify
+  "Return a seq of maps like {:name \"Edward Cullen\" :glitter-index 10}"
+  [rows]
+  (map (fn [unmapped-row]
+         (reduce (fn [row-map [vamp-key value]]
+                   (assoc row-map vamp-key (convert vamp-key value)))
+                 {}
+                 (map vector vamp-keys unmapped-row)))
+       rows))
+
+(defn glitter-filter
+  [minimum-glitter records]
+  (filter #(>= (:glitter-index %) minimum-glitter) records))
+
+(defn names
+  [suspect-maps]
+  (map :name suspect-maps))
+
+(defn append
+  [suspects new-suspect]
+  (conj suspects new-suspect))
+
+(defn validate
+  [validations suspect]
+  (every? (fn [[key predicate]]
+            (and (key suspect)
+                 (predicate (key suspect))))
+          validations))
+
+(defn back-to-csv
+  [suspects]
+  (clojure.string/join "\n"
+                       (map (fn [suspect]
+                              (->>
+                               (map suspect vamp-keys)
+                               (clojure.string/join ",")))
+                            suspects)))
---
+++ ch4/test/ch4/core_test.clj
@@ -0,0 +1,40 @@
+(ns ch4.core-test
+  (:require [clojure.test :refer :all]
+            [ch4.core :refer :all]))
+
+(def edward   {:name "Edward Cullen"   :glitter-index 10})
+(def jacob    {:name "Jacob Black"     :glitter-index 3})
+(def carlisle {:name "Carlisle Cullen" :glitter-index 6})
+(def bella    {:name "Bella Swan"      :glitter-index 0})
+(def charlie  {:name "Charlie Swan"    :glitter-index 0})
+
+(def suspects [edward jacob carlisle bella charlie])
+
+(deftest test-names
+  (is (= [(:name edward)
+          (:name jacob)
+          (:name carlisle)
+          (:name bella)
+          (:name charlie)]
+         (names suspects))))
+
+(deftest test-append
+  (is (= suspects
+         (append [edward jacob carlisle bella] charlie))))
+
+(deftest test-validate
+  (are [validations suspect valid] (= valid (validate validations suspect))
+    {:name string? :key-missing even?} bella false
+    {:name string? :glitter-index integer?} edward true
+    {:name string? :glitter-index integer?} jacob true))
+
+
+(deftest test-back-to-csv
+  (let [expected-csv
+        (str "Edward Cullen,10\n"
+             "Jacob Black,3\n"
+             "Carlisle Cullen,6\n"
+             "Bella Swan,0\n"
+             "Charlie Swan,0")]
+    (is (= expected-csv
+           (back-to-csv suspects)))))
